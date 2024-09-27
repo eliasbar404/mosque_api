05a80221db5b5856cf77417c0057b2ad7ece4aab
ilyas <ibaraaouche@gmail.com>
2024-09-27 10:29:05 +0100

    add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Admin;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Admin::factory()->create([
+            'name' => 'elias',
+            'email' => 'elias@2000222',
+            'password' => bcrypt("elias@2000222"),
         ]);
     }
 }
